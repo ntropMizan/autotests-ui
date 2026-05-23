35413014e5b5d64f0311bd4407dd9ab7465fbf78
Mikhail <brooklyndays@mail.ru>
2026-05-23 12:22:33 +0300

    Практика по углубленной работе с Playwright

---
+++ playwright_registration_button.py
@@ -0,0 +1,25 @@
+import time
+
+from playwright.sync_api import sync_playwright, expect
+
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    registration_link = page.get_by_test_id('registration-page-registration-button')
+    expect(registration_link).to_be_disabled()
+
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
+
+    user_name_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    user_name_input.fill('username')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
+
+    expect(registration_link).to_be_enabled()
+
