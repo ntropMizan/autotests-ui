f167eb4558d9047453342beb1ec78a295431ae3b
Mikhail <brooklyndays@mail.ru>
2026-05-23 14:21:22 +0300

    практика с авторизацией

--- playwright_courses.py
+++ playwright_courses.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 
 
 with sync_playwright() as playwright:
@@ -20,9 +20,7 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-
     context.storage_state(path='browser-state.json')
-    page.wait_for_timeout(5000)
 
 
 with sync_playwright() as playwright:
@@ -30,6 +28,23 @@ with sync_playwright() as playwright:
     context = browser.new_context(storage_state='browser-state.json')
     page = context.new_page()
 
+
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
+    courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text('Courses')
+
+    alert_no_result = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(alert_no_result).to_be_visible()
+    expect(alert_no_result).to_have_text('There is no results')
+
+    icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(icon).to_be_visible()
+
+    alert_no_result_text = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(alert_no_result_text).to_be_visible()
+    expect(alert_no_result_text).to_have_text('Results from the load test pipeline will be displayed here')
+
     page.wait_for_timeout(5000)
+
