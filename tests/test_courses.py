721d974e689d0a2c6ae3b1278dda2da62f2b63b7
Mikhail <brooklyndays@mail.ru>
2026-09-06 22:13:24 +0300

    vdfgdfgbxcvcvUpdate Jenkinsfile with requirements

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -10,6 +10,7 @@ from pages.create_course_page import CreateCoursePage
 @allure.feature("Create Course")
 @allure.story("Create new course")
 @allure.title("Создание нового курса")
+@allure.id('15')
 @allure.severity(allure.severity_level.CRITICAL)
 @pytest.mark.courses
 @pytest.mark.regression
